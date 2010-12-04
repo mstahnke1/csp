2c9732ff9f9d9a7e10fa23da30ae1845515bd2bb
mstahnke1@gmail.com <mstahnke1@gmail.com@32a86564-0d59-ff81-cd25-72881f0217ef>
2010-12-04 18:49:24 +0000

    cleaning trunk. replacing trunk with the 3.0 version. There were problems with directory structure.

--- trunk/ver3/cspInfoPanel.php
+++
@@ -1,4 +0,0 @@
-<img src="images/logo_home.gif" alt="Company Logo" style="float:left;" />
-<div class="cspInfoPanel">Welcome Mike Stahnke<br />Date: Wed, Nov 24, 2010
-	<div class="cspInfoPanelMsg">Happy Thanksgiving!</div>
-</div>
--- trunk/ver3/cspMenuBar.php
+++
@@ -1,4 +0,0 @@
-<div class="menuBox box1" <?php if(basename($_SERVER['PHP_SELF'], ".php") == "cspUserHome_Dashboard") { echo 'style="border:2px solid black;"'; } ?> onClick="self.location.href='cspUserHome_Dashboard.php'">Home</div>
-<div class="menuBox box2" <?php if(basename($_SERVER['PHP_SELF'], ".php") == "cspUserSupport_Home") { echo 'style="border:2px solid black;"'; } ?> onClick="self.location.href='cspUserSupport_Home.php'">Support</div>
-<div class="menuBox box3" <?php if(basename($_SERVER['PHP_SELF'], ".php") == "cspUserSales_Home") { echo 'style="border:2px solid black;"'; } ?> onClick="self.location.href='cspUserSales_Home.php'">Sales</div>
-<div class="menuBox box4" <?php if(basename($_SERVER['PHP_SELF'], ".php") == "cspAdmin_Home") { echo 'style="border:2px solid black;"'; } ?> onClick="self.location.href='cspAdmin_Home.php'">Admin</div>
